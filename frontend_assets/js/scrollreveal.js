3cb21b4bcfec3c9669665463ac7f8b32f44301c2
Fenar Khan <fenarkhan7@gmail.com>
2023-09-21 12:02:17 +0530

    Adeed Files

---
+++ frontend_assets/js/scrollreveal.js
@@ -0,0 +1,2 @@
+ScrollReveal().reveal('.sports-inner',{delay:200,easing:'ease-out',interval:200,scale:1.2})
+ScrollReveal().reveal('.pdf-version',{delay:200,easing:'ease-out',interval:200,scale:1.2})
